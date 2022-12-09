af296dbc894e50cab72aa848ce40bf20f774638d
Namaste710 <HoaiNamNguyen710@googlemail.com>
2022-12-09 20:47:10 +0100

    added IAM rights to policy and tested connection

--- Web Apps/auto-mpg/training/main.py
+++ Web Apps/auto-mpg/training/main.py
@@ -1,5 +1,9 @@
 import training as training
 import joblib
+import pickle
+from minio import Minio
+import io
+
 FILE_PATH = "data/auto-mpg.data"
 MODEL_PATH = "artifacts/model.joblib"
 
@@ -7,5 +11,21 @@ if __name__ == "__main__":
     df = training.load_data(FILE_PATH)
     model, preproc_pl = training.model_training(df)
     joblib.dump((model, preproc_pl), MODEL_PATH)
+    
+    minio_client = Minio(endpoint="minio-storage:9000")
+    
+    bytes_model_pipeline = pickle.dumps((model, preproc_pl))
+    
+    minio_client.put_object(
+            bucket_name="test-bucket",
+            object_name="test",
+            data=io.BytesIO(bytes_model_pipeline),
+            length=len(bytes_model_pipeline)
+        )
+    
+    # pickle.loads(client.get_object(bucket_name=bucket_name,
+    #                            object_name=path_file).read())
+    
+    
     
     
