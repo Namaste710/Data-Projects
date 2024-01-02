16f59c2c356399efc2ba79fa679f2a88fc7089fe
Hoai-Nam Nguyen <hoai-nam.nguyen@diconium.com>
2024-01-02 21:06:11 +0100

    feat: added asset and job for yahoo finance data

--- yahoo-finance-data-pipeline/services/dagster/pipeline/ingest.py
+++ yahoo-finance-data-pipeline/services/dagster/pipeline/ingest.py
@@ -1,50 +1,78 @@
-import time
-
-from dagster import FilesystemIOManager, graph, job, op, repository, schedule
-from dagster_docker import docker_executor
+import re
+from datetime import datetime, timedelta
+from typing import List
 
 import yfinance as yf
-
-
-@op
-def hello():
-    return 1
-
-
-@op
-def goodbye(foo):
-    return foo * 2
-
-
-@op
-def hanging_op():
-    while True:
-        time.sleep(5)
-
-
-@job
-def hanging_job():
-    hanging_op()
-
-
-@graph
-def my_graph():
-    goodbye(hello())
-
-
-my_job = my_graph.to_job(name="my_job")
-my_step_isolated_job = my_graph.to_job(
-    name="my_step_isolated_job",
-    executor_def=docker_executor,
-    resource_defs={"io_manager": FilesystemIOManager(base_dir="/tmp/io_manager_storage")},
+from dagster import (
+    AssetExecutionContext,
+    Config,
+    Definitions,
+    MaterializeResult,
+    MetadataValue,
+    RunConfig,
+    asset,
+    define_asset_job,
 )
 
 
-@schedule(cron_schedule="* * * * *", job=my_job, execution_timezone="US/Central")
-def my_schedule(_context):
-    return {}
+def yesterday() -> str:
+    yesterday = datetime.now() - timedelta(1)
+    return datetime.strftime(yesterday, "%Y-%m-%d")
 
 
-@repository
-def deploy_docker_repository():
-    return [my_job, hanging_job, my_schedule, my_step_isolated_job]
+class YahooTickerConfig(Config):
+    start_date: str
+    end_date: str
+    symbols: List[str]
+
+
+@asset
+def fetch_ticker_data(
+    context: AssetExecutionContext, config: YahooTickerConfig
+) -> MaterializeResult:
+    symbols = config.symbols
+    start_date = config.start_date
+    if not re.match(start_date, "^[0-9]{4}-[0-9]{2}-[0-9]{2}$"):
+        start_date = "2000-01-01"
+    end_date = config.end_date
+    if not re.match(end_date, "^[0-9]{4}-[0-9]{2}-[0-9]{2}$"):
+        end_date = yesterday()
+
+    tickers = yf.Tickers(symbols)
+    tickers_hist_df = tickers.history(start=start_date, end=end_date)
+
+    # TRANSFORM MULTI-LEVEL INDEX INTO A SINGLE-INDEX SET OF COLUMNS.
+    tickers_hist_df = (
+        tickers_hist_df.stack(level=1)
+        .rename_axis(["Date", "Ticker"])
+        .reset_index(level=1)
+    )
+
+    context.log.info(f"DataFrame has {len(tickers_hist_df)} rows.")
+
+    return MaterializeResult(
+        metadata={
+            "num_records": len(tickers_hist_df),
+            "preview": MetadataValue.md(tickers_hist_df.head().to_markdown()),
+        }
+    )
+
+
+yahoo_ticker_job = define_asset_job(
+    "yahoo_ticker_job",
+    selection="fetch_ticker_data",
+    config=RunConfig(
+        {
+            "fetch_ticker_data": YahooTickerConfig(
+                start_date="2000-01-01",
+                end_date="2024-01-01",
+                symbols=["AAPL", "GOOGL", "ORCL", "MSFT", "IBM", "AMZN"],
+            )
+        }
+    ),
+)
+
+defs = Definitions(
+    assets=[fetch_ticker_data],
+    jobs=[yahoo_ticker_job],
+)
